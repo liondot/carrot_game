7252d5dcd6fa06ef49d745176b62626142d33563
liondot <51257576+liondot@users.noreply.github.com>
2022-06-24 19:20:39 +0900

    리플레이 버튼

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ let started = false;
 let score = 0;
 let timer = undefined;
 
+field.addEventListener('click', onFieldClick);
 gameBtn.addEventListener('click', ()=> {
   if(started) {
     stopGame();
@@ -26,10 +27,16 @@ gameBtn.addEventListener('click', ()=> {
     startGame();
   }
 
-  started = !started
+
+})
+
+popUpRefresh.addEventListener('click', ()=> {
+  startGame();
+  hidePopUp();
 })
 
 function startGame(){
+  started = true;
   initGame();
   showStopButton();
   showTimerAndScore();
@@ -37,13 +44,21 @@ function startGame(){
 }
 
 function stopGame(){
+  started = false;
   stopGameTimer();
   hideGameButton();
   showPopUpWithText('REPLAY?');
 }
 
+function finishedGame(win) {
+  started = false;
+  hideGameButton();
+  showPopUpWithText(win? 'YOU WON!!!' : 'YOU LOST!!!')
+
+}
+
 function showStopButton(){
-  const icon = gameBtn.querySelector('.fa-play');
+  const icon = gameBtn.querySelector('.fa-solid');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play')
 }
@@ -63,6 +78,7 @@ function startGameTimer() {
   timer = setInterval(() => {
     if(remainingTimeSec <= 0) {
       clearInterval(timer);
+      finishedGame(CARROT_COUNT === score);
       return;
     }
     updateTimerText(--remainingTimeSec)
@@ -76,6 +92,10 @@ function stopGameTimer() {
 function showPopUpWithText(text) {
   popUpText.innerText = text;
   popUp.classList.remove('pop-up--hide');
+}
+
+function hidePopUp() {
+  popUp.classList.add('pop-up--hide')
 }
 
 function updateTimerText(time) {
@@ -92,6 +112,33 @@ function initGame() {
   gameScore.innerText = CARROT_COUNT;
   addItem('carrot', CARROT_COUNT, 'imgs/carrot.png');
   addItem('bug', BUG_COUNT, 'imgs/bug.png');
+}
+
+function onFieldClick(event){
+  if(!started) {
+    return;
+  }
+  const target = event.target;
+  if(target.matches('.carrot')) {
+    //  당근 
+    target.remove();
+    score++;
+    updateScoreBoard();
+    if(score === CARROT_COUNT) {
+      finishedGame(true)
+    }
+
+  } else if (target.matches('.bug')){
+    // 버그 
+    stopGameTimer();
+    finishedGame(false);
+  }
+}
+
+
+
+function updateScoreBoard() {
+  gameScore.innerText = CARROT_COUNT - score;
 }
 
 function addItem(className, count, imgPath) {
